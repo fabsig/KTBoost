69dcf7fc9284ff751957f1d89dcbc39ceabb077c
fabsig <32902180+fabsig@users.noreply.github.com>
2021-01-14 13:19:19 +0100

    add boosting with Gamma loss demo

---
+++ examples/Gamma_regression_demo.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 14 11:54:35 2021
+
+@author: Fabio Sigrist
+
+Small demo of Gamma regression, just to illustrate to code workflow
+"""
+
+import sklearn.datasets as datasets
+import numpy as np
+import KTBoost.KTBoost as KTBoost
+import matplotlib.pyplot as plt
+
+# Simulate data
+np.random.seed(1)
+n = 10000
+X, lp = datasets.make_friedman3(n_samples=n)
+X_test, lp_test = datasets.make_friedman3(n_samples=n)
+lp = np.exp(lp/10)
+lp_test = np.exp(lp_test/10)
+y = np.random.gamma(scale=lp, shape=1)
+plt.hist(y)
+plt.show()
+
+# Train model and make predictions
+model = KTBoost.BoostingRegressor(loss='gamma').fit(X, y)
+pred_gamma = model.predict(X_test)
+# mean square error (approx. 0.026)
+print("Test error of Gamma boosting: " + str(((pred_gamma-lp_test)**2).mean()))
+plt.scatter(pred_gamma, lp_test)
+plt.title("True vs. predicted values")
+plt.show()
+
+# Compare to predictions of classical least squares gradient boosting
+model = KTBoost.BoostingRegressor(loss='ls').fit(X, y)
+pred_ls = model.predict(X_test)
+# mean square error (approx. 0.039)
+print("Test error of standard least squares gradient boosting: " + str(((pred_ls-lp_test)**2).mean()))
+plt.scatter(pred_gamma, pred_ls)
+plt.title("Least squares gradient boosting vs. Gamma boosting predicted values")
+plt.show()
